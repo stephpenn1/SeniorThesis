7af72064695091a66b1121274710422bf524f34f
stephpenn1 <SP@Stephanies-MacBook-Pro.local>
2016-07-19 14:18:30 -0400

    overlay contour map with image
    lines 31-33 adds subtitle to image plot
    
    lines 44-48 attempted to combines the color image of pot density with
    contour lines. axes arguments do not agree when plotted

--- quick_ncdf_script.R
+++ quick_ncdf_script.R
@@ -8,7 +8,7 @@ lon <-ncvar_get(ncid,"lon")
 lat <-ncvar_get(ncid,"lat")
 time <-ncvar_get(ncid,"time")
 depth <- ncvar_get(ncid, "lev")
-x<-mean_rho-1000
+x<-mean_rho-1000 #changes the density to 2 sig figs
 
 #let's pull out the first level and the first date
 rho_surf<-rho[, , 1, 1:12]  # the means we want all lat, all lon, the 1st depth and 1-12 months 
@@ -19,16 +19,18 @@ mean_rho<-apply(rho_surf, c(1,2), mean) # this should return a 2-d matrix
 #let's make lat and lon only 1 dimension
 lat_1<-lat[1,]
 lon_1<-lon[,1]
-lon_sort<-sort(lon_1)
+lon_sort<-sort(y)
 
-y<-lon_1-180
+y<-lon_1-180  #changes the scale to -180/+180
 
 #quickly plot it
 image(lon_sort,lat_1,x, 
       xlab= "Longitude", 
       ylab="Latitude", #axes labels
       main="Potential Density", #main plot title
-      col = heat.colors(15, alpha =1)) #color palette
+      sub = "Plot of potential density in 2006",
+      font.sub = 3, col.sub = "cornflowerblue",
+      col = topo.colors(15, alpha =1)) #color palette
 
 # or
 contour(lon_sort,lat_1,x, 
@@ -38,4 +40,10 @@ contour(lon_sort,lat_1,x,
 
 # we see that the contours are 1030 ish which is a good density number. 
 
+
+image(lon_sort,lat_1,x,
+      col = topo.colors(15)) #color palette
+par(new = T)  #combines plots together
+contour(lon_sort,lat_1,x)
+par(new = F)
 
