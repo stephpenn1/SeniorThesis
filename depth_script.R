0249e2539dd30f5b78200eec6c4b5e65185e1609
stephpenn1 <SP@Stephanies-MacBook-Pro.local>
2016-09-29 16:26:07 -0400

    revised to reflect fixed long issue

--- depth_script.R
+++ depth_script.R
@@ -1,28 +1,32 @@
 #Stephanie Pennington | Senior Thesis
 #Historical(1970-1990) and RCP8.5 (2030-2050)
 #Salinity/Potential Density Data CMIP5 CESM1-BGC
-#Created 8.9.2016
-# --------------------------------
-# ---- DEPTH PLOTS w CONTOURS ----
-# --------------------------------
+#Created 8-9-16 | Revised 9-29-16
 
-setwd("/Users/SP/Desktop/average-outputs") #set working directory to files
+# ---- DEPTH PLOTS w CONTOURS ----
+
+#set working directory
+setwd("/Users/SP/Desktop/cmip5_files")
 
 ## --------------OPEN DATA ------------------
+
 library(ncdf4)
+
 # First we need to open the NetCDF files after it has gone through CDO
-ncid <- nc_open("so_havg_output.nc")
-ncid2 <- nc_open("rhopoto_havg_output.nc")
+
+ncid <- nc_open("rhopoto_hist_19701990.nc_regridfile.nc")
+ncid2 <- nc_open("salt_hist_19701990.nc_regridfile.nc")
 
 # RCP8.5 2030-2050 outputs
-#ncid <- nc_open("so_r85avg3050_output.nc") 
-#ncid2 <- nc_open("rhopoto_r85avg3050_output.nc")
+#ncid <- nc_open("rhopoto_rcp85_20302050.nc_regridfile.nc")
+#ncid2 <- nc_open("salt_rcp85_20302050.nc_regridfile.nc")
 #this file has already been merged and the time variable averaged
+
 #print(ncid)
 
 #pull out variables
-salt <-ncvar_get(ncid, "so")
-rho <-ncvar_get(ncid2, "rhopoto")
+salt <-ncvar_get(ncid2, "so")
+rho <-ncvar_get(ncid, "rhopoto")
 lon <-ncvar_get(ncid,"lon")
 lat <-ncvar_get(ncid,"lat")
 time <-ncvar_get(ncid,"time")
@@ -31,31 +35,25 @@ depth <- ncvar_get(ncid, "lev")
 ## --------------PLOT DATA------------------
 library(fields)
 
-#here we want all depths and only one specific longitude
+#want all depths and one longitude
 salt_depth<-salt[222, , ]  #only three variables since time was averaged out
-#mean_salt<-apply(salt_depth,c(1,2), mean) # this should return a 2-d matrix
-salt_u<- salt_depth*1000 #changes units to 3 sig figs
+salt_u<- salt_depth*1000 #change units to 3 sig figs
 
-rho_depth<-rho[222, , ]  
-#mean_rho<-apply(rho_depth, c(1,2), mean) #takes mean of time
-rho_u<- rho_depth-1000 #changes units to 2 sig figs
+rho_depth<-rho[222, , ]
+rho_u<- rho_depth-1000 #change units to 2 sig figs
 
-#let's make lat and lon only 1 dimension
-lat_1<-lat[1,]
-lon_1<-lon[,1]
-lon_sort<-sort(lon_1) #sorts jumbled longitudes **needs to be fixed**
 
-#plot the data; the first dataset is in color and the second is contoured on top
-image.plot(lat_1,depth,salt_u,
-      xlab= "latitude", 
-      ylab="depth [M]",
-      sub = "150W 1970-1990",
-      font.sub = 3, col.sub = "turquoise4",
-      xlim = c(-75,0), #only want equator to 75S
-      ylim = c(2000,0), #only want the top 2000 meters
-      zlim = c(33.5,35), #changes colorbar range
-      col = rainbow(150, start = .1, end = .85, alpha =1)) #uses rainbow palette with 150 colors, hue ranges from .1 to .85
-title(main = "Salinity with Potential Density Contours \n(psu) (kg/m^3)")
-par(new = T)  #overlay plots
-contour(lat_1,depth,rho_u, nlevels = 25, add= TRUE) #contour plot with 25 levels
+#plot data
+image.plot(lat,depth,salt_u,
+           xlab= "latitude", 
+           ylab="depth [M]",
+           sub = "150W 1970-1990 (contour levels are in increments of 0.2 kg/m^3)",
+           font.sub = 3, col.sub = "brown1",
+           xlim = c(-75,0), #only want equator to 75S
+           ylim = c(2000,0), #only want top 2000 meters
+           zlim = c(33.5,35), #changes colorbar range
+           col = rainbow(150, start = .15, end = .75, alpha =1)) #uses rainbow palette with 150 colors, hue ranges from .1 to .85
+title(main = "Historic Salinity and Density in the Southern Pacific") #(kg/m^3)
+par(new = T)
+contour(lat_1,depth,rho_u, nlevels = 30, drawlabels = TRUE, add= TRUE)
 par(new = F) #end overlay
--- surface_script.R
+++ surface_script.R
@@ -1,57 +1,61 @@
 #Stephanie Pennington | Senior Thesis
 #Historical(1970-1990) and RCP8.5 (2030-2050)
-#Salinity/Potential Density Data - CMIP5 CESM1-BGC 
-#Created 8.11.2016
+#CMIP5 CESM1-BGC Data
+#Created 8-11-2016 | Revised 9-29-16
+
 # --------------------------------
 #SURFACE PLOTS w CONTOURS
 # --------------------------------
 
-setwd("/Users/SP/Desktop/average-outputs") #set working directory to files
+setwd("/Users/SP/Desktop/cmip5_files") #set working directory to files
 
 ## --------------OPEN DATA ------------------
 library(ncdf4)
-# First we need to open the NetCDF files after it has gone through CDO
-ncid <- nc_open("so_havg_output.nc")
-ncid2 <- nc_open("rhopoto_havg_output.nc")
 
-# RCP8.5 2030-2050 outputs
-#ncid <- nc_open("so_r85avg3050_output.nc")
-#ncid2 <- nc_open("rhopoto_r85avg3050_output.nc")
-#this file has already been merged and the time variable averaged
+# open the NetCDF files after it has gone through CDO
+ncid <- nc_open("thetao_hist_19701990.nc_regridfile.nc")
+
+#RCP8.5 2030-2050 outputs
+#ncid <- nc_open("thetao_rcp85_20302050.nc_regridfile.nc")
+
 #print(ncid)
 
 #pull out variables
-salt <-ncvar_get(ncid, "so")
-rho <-ncvar_get(ncid2, "rhopoto")
+#salt <-ncvar_get(ncid, "so")
+#rho <-ncvar_get(ncid, "rhopoto")
+#oxy <-ncvar_get(ncid,"o2")
+temp <-ncvar_get(ncid, "thetao")
 lon <-ncvar_get(ncid,"lon")
 lat <-ncvar_get(ncid,"lat")
 time <-ncvar_get(ncid,"time")
-depth <- ncvar_get(ncid, "lev")
+#depth <- ncvar_get(ncid, "lev")
 
 ## --------------PLOT DATA------------------
 library(fields)
 
 #pull out only the first level
-salt_surf<-salt[ , , 1] #there are only three variables now instead of four since time was averaged out
-#mean_salt<-apply(salt_surf, c(1,2), mean) #takes mean of time
-salt_u<- salt_surf*1000 #changes units to 3 sig figs
 
-rho_surf<-rho[ , ,1]  
-#mean_rho<-apply(rho_surf, c(1,2), mean)
-rho_u<- rho_surf-1000 #changes units to 2 sig figs
+#oxy_surf<-oxy[ , 1]
+#salt_surf<-salt[ , , 1]
+#salt_u<- salt_surf*1000 #change units to 3 sig figs
+#rho_surf <- rho[ , , 1]
+#rho_u<- rho_surf-1000
 
-#let's make lat and lon only 1 dimension
-lat_1<-lat[1,]
-lon_1<-lon[,1]
-lon_sort<-sort(lon_1) #sorts jumbled longitudes **needs to be fixed**
+#there are only three variables now instead of four since time was averaged out
+temp_surf<-temp[ , , 1]
+temp_u<- temp_surf-273.15 #change units to celsius
 
 #plot the data; the first dataset is in color and the second is contoured on top
-image.plot(lon_sort,lat_1,salt_u, 
-           xlab= "Longitude", 
-           ylab= "Latitude",
-           sub = "Average Global Salinity 1970-1990",
-           font.sub = 3, col.sub = "firebrick2") #axes labels
-title(main = "Historical Salinity with Potential Density Overlay \n(psu) (kg/m^3)")
+image.plot(lon,lat,temp_u, 
+           xlab = "Longitude", 
+           ylab = "Latitude",
+           ylim=rev(c(0 , -90)),
+           xlim=c(150,300),
+           #zlim = c(33,34),
+           sub = "Average Historic SST 1970-1990",
+           font.sub = 3, col.sub = "firebrick2")
+           #col = rainbow(150, start = .15, end = .75, alpha =1)) #axes labels
+title(main = "Average Historic Surface Temperature in Southern Pacific\n (Celsius)") #kg/m^3
 par(new = T)
-contour(lon_sort,lat_1,rho_u, nlevels = 15, add = TRUE) #overlay contours
+contour(lon,lat,temp_u, nlevels = 10, add = TRUE) #overlay contours
 par(new = F)
