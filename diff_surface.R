ada0764d834d307d093ada2a9346057e692a31bb
stephpenn1 <SP@Stephanies-MBP.home>
2016-11-01 14:32:26 -0400

    rcp contours added

--- diff_surface.R
+++ diff_surface.R
@@ -7,59 +7,68 @@
 
 library(ncdf4)
 library(fields)
-ncid1 <- nc_open("thetao_hist_19701990.nc_regridfile.nc")
-ncid2 <- nc_open("so_hist_19701990.nc_regridfile.nc")
+ncid1 <- nc_open("rhopoto_hist_19701990.nc_regridfile.nc")
+#ncid2 <- nc_open("rhopoto_hist_19701990.nc_regridfile.nc")
 
 #print(ncid)
-temp_hist<-ncvar_get(ncid1, "thetao")
-salt_hist<-ncvar_get(ncid2, "so")
-lon <-ncvar_get(ncid,"lon")
-lat <-ncvar_get(ncid,"lat")
-time <-ncvar_get(ncid3,"time")
-depth <- ncvar_get(ncid, "lev")
+#temp_hist<-ncvar_get(ncid1, "thetao")
+#salt_hist<-ncvar_get(ncid1, "so")
+rho_hist<-ncvar_get(ncid1, "rhopoto")
+lon <-ncvar_get(ncid1,"lon")
+lat <-ncvar_get(ncid1,"lat")
+time <-ncvar_get(ncid1,"time")
+depth <- ncvar_get(ncid1, "lev")
 
 #let's pull out the first level and the first date
 #o2_depth<-oxy_hist[ , , ]
-salt_surf_hist<-salt_hist[ , ,1] 
-salt_u_hist<- salt_surf_hist*1000
+#salt_surf_hist<-salt_hist[ , ,1] 
+#salt_u_hist<- salt_surf_hist*1000
 
-temp_surf_hist<-temp_hist[ , ,1]
-temp_u_hist<- temp_surf_hist-273.15 #changes units
+#temp_surf_hist<-temp_hist[ , ,1]
+#temp_u_hist<- temp_surf_hist-273.15 #changes units to Celsius
+
+rho_surf_hist <- rho_hist[ , , 1]
+rho_u_hist<- rho_surf_hist-1000
 
 ### RCP8.5
 
-ncid3 <- nc_open("thetao_rcp85_20802100.nc_regridfile.nc")
-ncid4 <- nc_open("so_rcp85_20802100.nc_regridfile.nc")
+ncid3 <- nc_open("rhopoto_rcp85_20802100.nc_regridfile.nc")
+#ncid4 <- nc_open("thetao_rcp85_20802100.nc_regridfile.nc")
 
-temp_rcp85<-ncvar_get(ncid3, "thetao")
-salt_rcp85<-ncvar_get(ncid4, "so")
-time <-ncvar_get(ncid,"time")
+#temp_rcp85<-ncvar_get(ncid3, "thetao")
+#salt_rcp85<-ncvar_get(ncid3, "so")
+rho_rcp85<-ncvar_get(ncid3, "rhopoto")
 
 #let's pull out the first level and the first date
 #o2_surf_rcp85<-oxy_rcp85[ , , ]  # the means we want all lat, one lon, all depths and 1-12 months
-salt_surf_rcp85<-salt_rcp85[ , ,1]
-salt_u_rcp85<- salt_surf_rcp85*1000
+#salt_surf_rcp85<-salt_rcp85[ , ,1]
+#salt_u_rcp85<- salt_surf_rcp85*1000
 
-temp_surf_rcp85<-temp_rcp85[ , ,1]
-temp_u_rcp85<- temp_surf_rcp85-273.15 
+#temp_surf_rcp85<-temp_rcp85[ , ,1]
+#temp_u_rcp85<- temp_surf_rcp85-273.15 
 
+rho_surf_rcp85 <- rho_rcp85[ , , 1]
+rho_u_rcp85<- rho_surf_rcp85-1000
 
 ## difference
-diff_temp <- temp_u_rcp85 - temp_u_hist
+diff_rho <- rho_u_rcp85 - rho_u_hist
 
 #save plot
-png(file = "sp_surf_thetao_diff2.png", width = 650, height = 300, units = "mm",res = 100)
-image.plot(lon,lat,diff_temp,
+#png(file = "sp_surf_thetao_diff2.png", width = 650, height = 300, units = "mm",res = 100)
+image.plot(lon,lat,diff_rho,
            xlab= "latitude", 
            ylab="longitude",
            ylim=rev(c(0 , -90)),
            xlim=c(150,300),
-           zlim=c(0,4))
+           zlim=c(-1,0.5))
            #sub = "")
-title(main = "South Pacific Surface Temperature Difference\n 1970-1990 - 2080-2100") 
-dev.off()
-#ylim=rev(c(-20 , -90)),
+title(main = "South Pacific Surface Density Difference\n 1970-1990 - 2080-2100") 
+#dev.off()
+#ylim=rev(c(-20,-90)),
 #xlim=c(200,360))
 par(new = T)
-contour(lon,lat,salt_u_hist, nlevels = 35, add = TRUE)
+z = c(27.0,27.2)
+contour(lon,lat,rho_u_hist, nlevels = 0, add = TRUE, zlim= range(z, finite = TRUE)) #hist contours
+par(new = T)
+contour(lon,lat,rho_u_rcp85, nlevels = 0, add = TRUE, lty = 4 , zlim= range(z, finite = TRUE)) #rcp85 contours, dashed
 par(new = F)
--- surface_script.R
+++ surface_script.R
@@ -13,7 +13,7 @@ setwd("/Users/SP/Desktop/cmip5_files") #set working directory to files
 library(ncdf4)
 
 # open the NetCDF files after it has gone through CDO
-ncid <- nc_open("thetao_hist_19701990.nc_regridfile.nc")
+ncid <- nc_open("so_hist_19701990.nc_regridfile.nc")
 
 #RCP8.5 2030-2050 outputs
 #ncid <- nc_open("thetao_rcp85_20302050.nc_regridfile.nc")
@@ -21,7 +21,7 @@ ncid <- nc_open("thetao_hist_19701990.nc_regridfile.nc")
 #print(ncid)
 
 #pull out variables
-#salt <-ncvar_get(ncid, "so")
+salt <-ncvar_get(ncid, "so")
 #rho <-ncvar_get(ncid, "rhopoto")
 #oxy <-ncvar_get(ncid,"o2")
 temp <-ncvar_get(ncid, "thetao")
@@ -30,14 +30,15 @@ lat <-ncvar_get(ncid,"lat")
 time <-ncvar_get(ncid,"time")
 #depth <- ncvar_get(ncid, "lev")
 
+
 ## --------------PLOT DATA------------------
 library(fields)
 
 #pull out only the first level
 
 #oxy_surf<-oxy[ , 1]
-#salt_surf<-salt[ , , 1]
-#salt_u<- salt_surf*1000 #change units to 3 sig figs
+salt_surf<-salt[ , , 1]
+salt_u<- salt_surf*1000 #change units to 3 sig figs
 #rho_surf <- rho[ , , 1]
 #rho_u<- rho_surf-1000
 
@@ -46,13 +47,13 @@ temp_surf<-temp[ , , 1]
 temp_u<- temp_surf-273.15 #change units to celsius
 
 #plot the data; the first dataset is in color and the second is contoured on top
-image.plot(lon,lat,temp_u, 
+image.plot(lon,lat,salt_u, 
            xlab = "Longitude", 
            ylab = "Latitude",
            #ylim=rev(c(0 , -90)), #S.Pacific
            #xlim=c(150,300), #S.Pacific
            #zlim = c(33,34),
-           sub = "Average Historic SST 1970-1990",
+           #sub = "Average Historic SST 1970-1990",
            font.sub = 3, col.sub = "firebrick2")
            #col = rainbow(150, start = .15, end = .75, alpha =1)) #axes labels
 title(main = "Average Historic Surface Temperature in Southern Pacific\n (Celsius)") #kg/m^3
